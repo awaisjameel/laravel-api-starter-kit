d8125ea4475a7d3fc211894de4838218c68f03f4
awaisjameel <awais.jameel@ymail.com>
2026-01-30 00:36:30 +0500

    feat: update CORS configuration to allow specific origins and enable credentials

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000',],
 
     'allowed_origins_patterns' => [],
 
@@ -39,6 +39,6 @@ return [
 
     'max_age' => 86400, // 24 hours
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
